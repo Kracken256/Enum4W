5ecb1c2eab15ee47ed9c834ed2188e831702f240
Wesley Jones <wesjones2004@gmail.com>
2022-10-04 00:28:04 -0400

    Added SSH key finding in user home.

--- enum4w.py
+++ enum4w.py
@@ -7,6 +7,7 @@ import math
 
 globals = {}
 
+
 def print_banner():
     print("+=============================================+")
     print("|          " +
@@ -101,16 +102,34 @@ def display_users(level: int):
     return
 
 
-def get_top_processes():
+def get_top_processes_by_mem():
     tmp = execute_cmd("ps aux | sort -nrk 3,3 | head -n 10")
     lines = tmp.splitlines()
     return lines
 
 
-def print_top_processes():
+def get_top_processes_by_cpu():
+    tmp = execute_cmd("ps aux | sort -nrk 4,4 | head -n 10")
+    lines = tmp.splitlines()
+    return lines
+
+
+def print_top_processes_by_mem():
+    print()
+    print(colored("Top 10 processes by MEM usage: ", "yellow"))
+    print(execute_cmd("ps aux | head -n 1").strip())
+    ps = get_top_processes_by_mem()
+    ps2 = []
+    for i in ps:
+        ps2.append(i[:120] + " ...")
+    print(colored('\n'.join(ps2), "green"))
+
+
+def print_top_processes_by_cpu():
+    print()
     print(colored("Top 10 processes by CPU usage: ", "yellow"))
     print(execute_cmd("ps aux | head -n 1").strip())
-    ps = get_top_processes()
+    ps = get_top_processes_by_cpu()
     ps2 = []
     for i in ps:
         ps2.append(i[:120] + " ...")
@@ -148,6 +167,8 @@ def print_files_in_root():
                      "initrd.img",
                      "vmlinuz",
                      "dev"
+                     "initrd.img.old",
+                     "vmlinuz.old"
                      ]
     interesting_files = [".dockerenv"]
     t1 = []
@@ -157,11 +178,12 @@ def print_files_in_root():
             t1.append(file)
         elif file.lower() not in default_files:
             t2.append(file)
-    result = colored('\t'.join(t1),"red") + colored('\t'.join(t2),"green") 
+    result = colored('\t'.join(t1), "red") + colored('\t'.join(t2), "green")
     print()
-    print(colored("Uncommon files in root: ","yellow"))
+    print(colored("Uncommon files in root: ", "yellow"))
     print(result)
     return
+
 
 def analyze_root_files():
     global globals
@@ -171,6 +193,7 @@ def analyze_root_files():
     # TODO: Add more checks later
     return
 
+
 def print_root_files_analysis():
     analyze_root_files()
     global globals
@@ -179,7 +202,81 @@ def print_root_files_analysis():
     if "IsDockerEnv" in globals.keys():
         if globals["IsDockerEnv"] == True:
             print(colored("Likely a container (Docker)", "green"))
+    else:
+        print("None")
     return
+
+
+def get_user_home_dirs():
+    dirs = os.listdir("/home")
+    t1 = []
+    for dir in dirs:
+        t1.append("/home/" + dir)
+    return t1
+
+
+def analyze_user_home_dirs():
+    global globals
+    dirs = get_user_home_dirs()
+    important_files = [".ssh"]
+    boring_if_empty = ["Desktop", "Documents", "Downloads",
+                       "Videos", "Music", "Pictures", "Public", "Templates"]
+    users_files = [[]]
+    globals["CheckSSHKeysInHomeDirectories"] = []
+    print()
+    print(colored("Analyzing user home directories", "yellow"))
+    null_print = True
+    for dir in dirs:
+        tmp = []
+        if os.access(dir, os.R_OK):
+            for file in os.listdir(dir):
+                filepath = f"{dir}/{file}"
+                if file in boring_if_empty:
+                    if len(os.listdir(filepath)) == 0:
+                        continue
+                elif file in important_files:
+
+                    if file == ".ssh":
+                        print(colored("Found .ssh directory in " + dir, "green"))
+                        globals["CheckSSHKeysInHomeDirectories"].append(
+                            filepath)
+                        null_print = False
+                else:
+                    tmp.append(filepath)
+        users_files.append(tmp)
+    if null_print:
+        print("None.")
+
+
+def check_if_ssh_key(filename: str):
+    try:
+        with open(filename, "r") as file:
+            contents = file.read()
+            if "-----BEGIN OPENSSH PRIVATE KEY-----" in contents and "-----END OPENSSH PRIVATE KEY-----" in contents:
+                return True
+    except:
+        print("Cant read file.")
+    return False
+
+
+def check_ssh_keys():
+    global globals
+    dirs_to_check = []
+    print()
+    print(colored("Finding SSH keys", "yellow"))
+    found_key = False
+    if "CheckSSHKeysInHomeDirectories" in globals.keys():
+        dirs_to_check += globals["CheckSSHKeysInHomeDirectories"]
+    for dir in dirs_to_check:
+        for file in os.listdir(dir):
+            if check_if_ssh_key(f"{dir}/{file}"):
+                print(
+                    colored(f"Found SSH key in {dir}/{file} Readable!!", "green"))
+                found_key = True
+    if not found_key:
+        print("No keys found")
+
+
 def run():
     print_banner()
     args = sys.argv
@@ -192,9 +289,12 @@ def run():
 
     if enum_level >= 1:
         display_users(enum_level)
-    print_top_processes()
+    print_top_processes_by_cpu()
+    print_top_processes_by_mem()
     print_files_in_root()
     print_root_files_analysis()
+    analyze_user_home_dirs()
+    check_ssh_keys()
     pass
 
 
